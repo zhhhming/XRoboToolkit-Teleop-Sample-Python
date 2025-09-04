f1fb7fdb58a6ddfebb032cd2b17b9f60cb437f7b
zhhhming <googoo000078@gmail.com>
2025-09-04 16:57:54 +0800

    12

--- xrobotoolkit_teleop/utils/path_utils.py
+++ xrobotoolkit_teleop/utils/path_utils.py
@@ -1,4 +1,4 @@
 import os
 
 PACKAGE_PATH = os.path.dirname(os.path.abspath(__file__))
-ASSET_PATH = os.path.join(PACKAGE_PATH, "../../assets")
+ASSET_PATH = "/home/ming/xrrobotics_new/XRoboToolkit-Teleop-Sample-Python/assets"
